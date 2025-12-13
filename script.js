28626ff721e3a240dd227303411a86a1677934ef
mohamed amine <mohamedaminewwe@gmail.com>
2025-12-13 02:00:14 +0100

    completed the script but still the style

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let num1
 let num2
 let operator
 
+/*variable to know if something is displayed*/
+let isResultDisplayed = false
+
 function add(num1,num2){
     return num1 + num2
 }
@@ -25,6 +28,7 @@ function modulo(num1,num2){
     return num1%num2
 }
 
+/*function to do the calculation ( takes two numbers and an operator )*/
 function operate(num1,num2,operator){
     if (operator==="+"){
         return add(num1,num2)
@@ -47,20 +51,35 @@ let displayElement = document.querySelector(".screen")
 let getbtn = document.querySelectorAll(".nums")
 let displayvalue = ""
 
+/*function to know wich number was pressed and to add it*/
 getbtn.forEach(button=>{
     button.addEventListener("click",()=>{
-        displayvalue += button.textContent
+        if (isResultDisplayed){
+            displayvalue = button.textContent
+            isResultDisplayed = false
+        }else{
+            displayvalue += button.textContent
+        }
         displayElement.textContent = displayvalue
     })
 })
 
 let getop = document.querySelectorAll(".operator")
 
+/*function to know wich operator was clicked and to do the operation if the user continues and not press equal*/
 getop.forEach(op=>{
     op.addEventListener("click",()=>{
-        if (num1 !== null && operator !== null){
+        if (isResultDisplayed){
+            num1 = Number(displayvalue)
+            operator = op.textContent
+            displayvalue = ""
+            isResultDisplayed = false
+        } else if  (num1 !== null && operator !== null && displayvalue !==""){
             num2 = Number(displayvalue)
             let result = operate(num1,num2,operator)
+            if (typeof result === "number"){
+                result = Math.round(result * 100000000)/100000000
+            }
             displayvalue = String(result)
             displayElement.textContent = displayvalue
             num1 = result
@@ -76,25 +95,36 @@ getop.forEach(op=>{
 
 let equal = document.querySelector(".equal")
 
+/*function to execute if user press equal button*/
 equal.addEventListener("click",()=>{
+    if (num1 === null || operator === null || displayvalue === ""){
+        return;
+    }
     num2 = Number(displayvalue)
-    displayvalue = String(operate(num1,num2,operator))
+    let result = operate(num1,num2,operator)
+    if (typeof result === "number"){
+        result = Math.round(result * 100000000)/100000000
+    }
+    displayvalue = String(result)
     displayElement.textContent = displayvalue
-
+    isResultDisplayed = true
 })
 
 let cl = document.querySelector(".cl")
 
+/*function to clear*/
 cl.addEventListener("click",()=>{
     displayvalue = ""
     num1 = null
     num2 = null
     operator = null
     displayElement.textContent = 0
+    isResultDisplayed = false
 })
 
 let ret = document.querySelector(".ret")
 
+/*function to return if user pressed the wrong number*/
 ret.addEventListener("click",()=>{
     if (displayvalue.length === 1){
         displayvalue = "0"
@@ -107,9 +137,10 @@ ret.addEventListener("click",()=>{
 
 let fl = document.querySelector(".fl")
 
+/*function to make float*/
 fl.addEventListener("click",()=>{
     if (!displayvalue.includes(".")){
-        displayvalue += "."
+        displayvalue += "0."
         displayElement.textContent = displayvalue
     }
 })
